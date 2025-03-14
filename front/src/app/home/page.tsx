818b3d297cd467016e35d44de48d36684818251c
nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2025-03-14 14:12:48 -0300

    home feita

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -20,25 +20,25 @@ const Home = () => {
                 <Image className="w-full object-contain" src={clould} alt="" />
                 </div>
             </div>
-            <div className="flex z-10 p-2 m-2 rounded-xl flex-col items-center justify-center bg-blue-200">
+            <div className="flex max-w-4/5 z-10 p-2 m-2 rounded-xl flex-col items-center justify-center bg-blue-200">
                 <Image className="bg-white w-110 rounded-xl m-2 p-2" src={window} alt="" />
                 
-                <div className="flex gap-3 rounded-xl">
+                <div className="flex w-full items-center justify-center max-w-4/5 gap-3 rounded-xl">
                 <Link href={ROUTES.home}>
-                    <Image className="max-w-30 h-auto cursor-pointer hover:scale-x-110 p-2" src={start} alt="" />
+                    <Image className="min-w-20 max-w-22 h-auto cursor-pointer hover:scale-x-110 p-2" src={start} alt="" />
                 </Link>
                 <Link href={ROUTES.home}>
-                    <Image className="max-w-30 h-auto cursor-pointer hover:scale-x-110 p-2" src={menu} alt="" />
+                    <Image className="min-w-20 max-w-22 h-auto cursor-pointer hover:scale-x-110 p-2" src={menu} alt="" />
                 </Link>
                 <Link href={ROUTES.login}>
-                    <Image className="max-w-30 h-auto cursor-pointer hover:scale-x-110 p-2" src={signIn} alt="" />
+                    <Image className="min-w-20 max-w-22 h-auto cursor-pointer hover:scale-x-110 p-2" src={signIn} alt="" />
                 </Link>
                 </div>
 
-                <div className="flex gap-3 items-center justify-center">
-                    <Image className="w-12 h-auto animate-bounce" src={arrow} alt="" />
+                <div className="flex gap-3 items-center justify-center max-w-4/5">
+                    <Image className="min-w-5 max-w-10 h-auto animate-bounce" src={arrow} alt="" />
                     <Link href={ROUTES.register} >
-                        <Image className="w-60 h-auto cursor-pointer hover:scale-x-110" src={account} alt="" />
+                        <Image className="min-w-10 max-w-40 h-auto cursor-pointer hover:scale-x-110" src={account} alt="" />
                     </Link>
                 </div>
             </div>
